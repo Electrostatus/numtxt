7f810a4969677f7e16c571244e8a5447b580cfc5
Electrostatus <electrostatus@gmail.com>
2018-05-26 15:50:44 -0400

    update year
    update copyright year

--- numtxt.py
+++ numtxt.py
@@ -1,5 +1,4 @@
 # numtxt - gives full and approximate written forms of numbers
-# Copyright (C) 2017  Electrostatus
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
